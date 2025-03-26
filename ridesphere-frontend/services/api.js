cc4609bcee122e315372e1f56fa305fd0c752ee5
hariharan0033 <hariharan00332810@gmail.com>
2025-03-26 23:50:18 +0530

    Updated API_URL in Frontend

--- ridesphere-frontend/services/api.js
+++ ridesphere-frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://10.0.2.2:5000/api";
-// const API_URL = "https://ridesphere-backend.vercel.app/";
+// const API_URL = "http://10.0.2.2:5000/api";
+const API_URL = "https://ridesphere-backend.vercel.app/api";
 
 export const api = axios.create({
   baseURL: API_URL,
